b2583e323b06b1d782a187926953524697d16234
Im2Slothy <69122728+Im2Slothy@users.noreply.github.com>
2021-05-27 18:18:51 -0500

    Add files via upload

---
+++ Python Terminal Game/Game.py
@@ -0,0 +1,72 @@
+# Intro to Terminal Story Game
+print("Welcome to the first game of mine made in python. FYI I started learning python on the 05/25/2021.\nStarting off I'm going to need you to answer some questions so that this can work! Answer them correctly or wrong I cant force you :). Enjoy!")
+
+#Variables used in game
+
+name = input("What name do you want to use?") 
+animal = input("What is your favorite animal?")
+food = input("What is your favorite food?")
+snack = input("What is your favorite snack")
+artist = input("What is your favorite music artist?")
+season = input("What is your favorite season of the year?")
+game = input("What is your favorite video game?")
+house = input("Would size house would you want if you could pick? (Ex: Small, Medium, Large, HUGE!")
+payment = input("What would you use to pay for things?")
+system = input("What is your favorite gaming system?")
+
+
+
+#Random question relating to github feedback
+
+print("First major thing is completely up to you! If you say no It makes no difference. If you click yes then I guess you're cool!")
+answer = input("Will you play my game and leave feedback if you wish?")
+if answer == "Yes" :
+    print("Ha, I really didn't expect that! Well thank you very much and enjoy!")
+if answer == "yes" :
+     print("Ha, I really didn't expect that! Well thank you very much and enjoy!")
+if answer == "YES" :
+     print("Ha, I really didn't expect that! Well thank you very much and enjoy!")
+
+else :
+    print("Thats alright! Well enjoy the game! :)")
+
+print("......................................................")
+print("......................................................")
+
+
+
+
+
+
+#Story started here
+print("Hello and welcome to the world "+name+"! I hope you enjoy the game!")
+print("......................................................")
+# Scene: Mr/Mrs:No Life
+print("Okay, the story all starts when you leave your "+house+" to get some fresh air and sunlight after spending the last 48 hours locked in your room playing "+game+". Yeah...\n **Thought flows through head** (Whats wrong with me?!?!). You then snap back into reality and walk to the corner store to get some snack and food.")
+print("......................................................")
+# Scene: Num Nums
+print("After the long walk to the store you walk inside and experience that nice store smell! You walk around the store getting items for yourself. In aisle 5 you find "+snack+" and in aisle 12 you find "+food+". You walk to the register and pay for your items. You grab your "+payment+" and pay the person behind the counter. You walkout and leave.")
+print("......................................................")
+# Scene: Home
+print("As you're walking back you think to yourself what a nice "+season+" it is. Once you get to your house you jump into bed and fall asleep almost instantly and start to dream.")
+print("......................................................")
+# Scene: The unimaginable
+print("You hear your favorite song by "+artist+" playing! You walk to a very fancy table in the middle of nowhere where your favorite "+food+" and "+snack+" are sitting very nicely prepared! You help yourself to all the food and eat all of it at what seems like the speed of light!")
+print("......................................................")
+# Scene: Exploration
+print("You finish your meal and begin to walk around in this strange place full of things you love. You a "+animal+" enjoying themself drinking water out of a nearby lake. You see "+artist+" playing your favorite song changing the words to match you!\n You think to yourself... (This is a dream isn't it!)\n💥BOOM💥")
+print("......................................................")
+# Scene: Back to normal
+print("You wake up and remember your dream VERY well. You can almost taste the "+snack+" and "+food+" that you ate.")
+print("......................................................")
+# Scene: Ending
+print("You get up off of your bed and walk to your "+system+" with "+game+" playing on the screen.\nNext thing you know you find yourself sitting down and the same cycle repeating.")
+print("......................................................")
+
+print("Hope you enjoyed!")
+print("If you have any questions contact me on discord!")
+print("Discord: Slothy#4484")
+print("I have a twitch if you want to follow that but its all up to you!")
+print("Twitch: Twitch.tv/Im2Slothy")
+print(" .▀█▀.█▄█.█▀█.█▄.█.█▄▀　█▄█.█▀█.█─█")
+print(" ─.█.─█▀█.█▀█.█.▀█.█▀▄　─█.─█▄█.█▄█")
